0438f0ddff2f8e89c565ddef81cbeaf0bc8b99d6
GARGI GHOSH <71563478+gargi321960@users.noreply.github.com>
2024-03-12 22:09:59 +0530

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,63 @@
+let selects = document.querySelectorAll(".select-container select"); 
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
+const but= document.querySelector("button");
+const amount = document.querySelector(".amount input");
+let ms=document.querySelector(".msg");
+fromCurr.value="USD";
+toCurr.value="IND";
+
+let updateExchangeRate = () =>
+{
+   // let amount = document.querySelector(".amount input");
+    var amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+      amtVal = 1;
+      amount.value = "1";
+    }
+    console.log(amtVal);
+
+}
+
+
+for(let drop of selects)
+{
+for(let x in countryList)
+{
+    let el = document.createElement("option");
+    el.textContent = x;
+    el.value =x;
+   drop.appendChild(el)
+}
+console.dir(drop);
+drop.addEventListener("change",() =>
+{
+  
+    let currCode = drop.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = drop.parentElement.querySelector("img");
+    img.src = newSrc;
+});
+}
+let calrate=()=>{
+but.addEventListener("click",(event) =>{
+event.preventDefault();
+fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurr.value}`)
+.then(response => response.json())
+.then(data => {
+  const conversionRate = data.rates[toCurr.value];
+  console.log(conversionRate);
+  ms.innerHTML=`${amount.value} ${fromCurr.value} equals ${conversionRate * amount.value} ${toCurr.value}`;
+  console.log(`${amount.value} ${fromCurr.value} equals ${conversionRate * amount.value} ${toCurr.value}`);
+})
+.catch(error => console.error('Error fetching conversion data:', error));
+  });
+}
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+    calrate();
+    fromCurr.value="USD";
+    toCurr.value="INR";
+  });
